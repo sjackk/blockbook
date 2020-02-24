1887250c2b268b1090101ec507e63de81c06bd80
sjackk <sjack0684@gmail.com>
2020-02-25 00:00:01 +0300

    Fixed merge after testing

--- server/public.go
+++ server/public.go
@@ -971,7 +971,22 @@ func (s *PublicServer) apiAddress(r *http.Request, apiVersion int) (interface{},
 	var address *api.Address
 	var err error
 	s.metrics.ExplorerViews.With(common.Labels{"action": "api-address"}).Inc()
-	page, pageSize, details, filter, _, _ := s.getAddressQueryParams(r, api.AccountDetailsTxidHistory, txsInAPI)
+
+	extended := false
+	e := r.URL.Query().Get("txs")
+	if len(e) > 0 {
+		extended, err = strconv.ParseBool(e)
+		if err != nil {
+			return nil, api.NewAPIError("Parameter 'txs' cannot be converted to boolean", true)
+		}
+	}
+
+	var accountDetailsType = api.AccountDetailsTxidHistory
+	if (extended) {
+		accountDetailsType = api.AccountDetailsTxHistoryLight
+	}
+
+	page, pageSize, details, filter, _, _ := s.getAddressQueryParams(r, accountDetailsType, txsInAPI)
 	address, err = s.api.GetAddress(addressParam, page, pageSize, details, filter)
 	if err == nil && apiVersion == apiV1 {
 		return s.api.AddressToV1(address), nil
